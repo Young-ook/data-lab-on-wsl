9854a35e98902800419b9556cf97afb7089ea226
Young-ook <youngook.kim@gmail.com>
2025-01-25 10:24:54 +0900

    fix(spark): failsafe code for spark_hoem

--- spark-ctl.sh
+++ spark-ctl.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
 # It is required to set the SPARK_HOME environment variable.
 # Please make sure the variable indicates to the right path to your spark.
-if [ -z "${SPARK_HOME}" ]; then
-  echo "SPARK_HOME is not configured. Please install spark and configure home path to environment variable."
+if [ -z $SPARK_HOME ]; then
+  echo "SPARK_HOME is NOT configured."
+  export SPARK_HOME="$HOME/.local/lib/spark-3.5.4-bin-hadoop3"
+  echo "$SPARK_HOME has been configuired.";
 fi
 
 ctl='run'
 
-function usage() {
+usage() {
   echo "Usage: $0 -r(run) -t(terminate)"
 }
 
-function args() {
-  if [[ $# < 1 ]]; then
+args() {
+  if [ $# -lt 1 ]; then
     usage
-    exit -1
+    exit 0
   fi
 
   while getopts "rt" opt; do
@@ -26,20 +28,20 @@ function args() {
       ;;
       \?)
         >&2 echo "Unrecognized argument '$OPTARG'"
-        print_usage
+        usage
         exit -1
       ;;
     esac
   done
 }
 
-function run() {
+run() {
   # run standalone cluster
   $SPARK_HOME/sbin/start-master.sh -h localhost
   $SPARK_HOME/sbin/start-worker.sh localhost:7077
 }
 
-function stop() {
+stop() {
   # terminate the running standalone cluster
   $SPARK_HOME/sbin/stop-worker.sh
   $SPARK_HOME/sbin/stop-master.sh
